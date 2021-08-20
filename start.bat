8cd0a44a1f869d420870db9028ab5f561daac879
Eranda Ravinath <65299296+eunlocker@users.noreply.github.com>
2021-08-20 12:09:02 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By Oshekher" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator lionsgaming /add >nul
+net user administrator williamns@992 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -16,6 +16,6 @@ echo Successfully Installed!, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: lionsgaming
+echo Password: williamns@992
 echo Please Login to your RDP!!
 ping -n 10 127.0.0.1 >nul
